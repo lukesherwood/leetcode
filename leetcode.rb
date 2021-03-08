696b54b51cead08a9e36ad8eb5bc733eca3cf961
lukesherwood <luke.sherwood55@gmail.com>
2021-03-08 11:46:04 -0500

    first solution to length of last word problem

--- leetcode.rb
+++ leetcode.rb
@@ -122,3 +122,35 @@ end
 
 puts find_ways("126")
 puts find_ways("12")
+
+# Given a string s consists of some words separated by spaces, return the length of the last word in the string. If the last word does not exist, return 0.
+
+# A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+# Example 1:
+
+# Input: s = "Hello World"
+# Output: 5
+
+# Example 2:
+
+# Input: s = " "
+# Output: 0
+
+ 
+
+# Constraints:
+
+#     1 <= s.length <= 104
+#     s consists of only English letters and spaces ' '.
+
+def length_of_last_word(s)
+    if s.empty? || s.strip.empty?
+        return 0
+    end
+    array = s.split(" ")
+    return array.last.length ? array.last.length : 0
+end
+
