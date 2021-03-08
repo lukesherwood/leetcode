67eb0e898b9e4d507cfd4ba57e84f85b711b025a
lukesherwood <luke.sherwood55@gmail.com>
2021-03-08 12:20:12 -0500

    solution to max sub array

--- leetcode.rb
+++ leetcode.rb
@@ -150,3 +150,45 @@ def length_of_last_word(s)
     s.split.last&.size || 0
 end
 
+# 53. Maximum Subarray
+# Easy
+
+# Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+ 
+
+# Example 1:
+
+# Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+# Output: 6
+# Explanation: [4,-1,2,1] has the largest sum = 6.
+
+# Example 2:
+
+# Input: nums = [1]
+# Output: 1
+
+# Example 3:
+
+# Input: nums = [0]
+# Output: 0
+
+# Example 4:
+
+# Input: nums = [-1]
+# Output: -1
+
+# Example 5:
+
+# Input: nums = [-100000]
+# Output: -100000
+
+
+def max_sub_array(nums)
+    largest_sum = nums.first
+    current_sum = 0
+    nums.each do |n| 
+        largest_sum = [largest_sum, current_sum = [n, current_sum+n].max].max
+    end
+    return largest_sum
+end
